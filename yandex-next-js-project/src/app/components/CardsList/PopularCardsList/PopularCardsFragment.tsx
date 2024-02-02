792d1b98a81d1bc1126f340cba04e9f96bed9455
RSA <ravils465@gmail.com>
2024-02-02 15:51:25 +0300

    commit

---
+++ yandex-next-js-project/src/app/components/CardsList/PopularCardsList/PopularCardsFragment.tsx
@@ -0,0 +1,58 @@
+import Style from './CardsList.module.css'
+
+export const PopularCardsFragment = () => {
+	return (
+		<>
+			<li className={Style["cards-list__item"]}>
+				<a href="/game-id" className={Style["card-list__link"]}>
+					<article className={Style["card"]}>
+						<img
+							src="https://code.s3.yandex.net/teens/pindie-games/dangeons-n-caves-prologue/cover.png"
+							alt=""
+							className={Style["card__image"]}
+						/>
+						<div className={Style["card__content-block"]}>
+							<h3 className={Style["card__title"]}>Dungeons`n`Caves. Prologue</h3>
+							<p className={Style["card__description"]}>
+								Безымянный герой исследует пещеры и подземелья, чтобы найти больше информации о себе.
+							</p>
+							<div className={Style["card__info-container"]}>
+								<p className={Style["card__author"]}>
+									Автор: <span className={Style["card__accent"]}>F-Style</span>
+								</p>
+								<p className={Style["card__votes"]}>
+									Голосов на сайте: <span className={Style["card__accent"]}>10</span>
+								</p>
+							</div>
+						</div>
+					</article>
+				</a>
+			</li>
+			<li className={Style["cards-list__item"]}>
+				<a href="/game-id" className={Style["card-list__link"]}>
+					<article className={Style["card"]}>
+						<img
+							src="https://code.s3.yandex.net/teens/pindie-games/defence-of-crystal/cover.png"
+							alt=""
+							className={Style["card__image"]}
+						/>
+						<div className={Style["card__content-block"]}>
+							<h3 className={Style["card__title"]}>Defence of Crystal</h3>
+							<p className={Style["card__description"]}>
+								Защищай магический кристалл от монстров и уничтожай кладбища, чтобы спасти Землю, которую поглотил мрак.
+							</p>
+							<div className={Style["card__info-container"]}>
+								<p className={Style["card__author"]}>
+									Автор: <span className={Style["card__accent"]}>MastWe</span>
+								</p>
+								<p className={Style["card__votes"]}>
+									Голосов на сайте: <span className={Style["card__accent"]}>20</span>
+								</p>
+							</div>
+						</div>
+					</article>
+				</a>
+			</li>
+		</>
+	)
+}
---
+++ yandex-next-js-project/src/app/components/CardsList/PopularCardsList/PopularCardsList.tsx
@@ -0,0 +1,20 @@
+import Style from './CardsList.module.css'
+import { PopularCardsFragment } from './PopularCardsFragment'
+
+export const PopularCardsList = () => {
+	return (
+		<section className={Style["list-section"]}>
+			<h2 className={Style["list-section__title"]} id="popular">
+				Популярное
+			</h2>
+			<ul className={Style["cards-list"]}>
+				{/* <li className="cards-list__item"> */}
+				{/* 	<a href="/game-id" className="card-list__link"> */}
+				{/* 		<Card/> */}
+				{/* 	</a> */}
+				{/* </li> */}
+				<PopularCardsFragment/>
+			</ul>
+		</section>
+	)
+}
